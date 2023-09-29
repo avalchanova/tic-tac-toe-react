63af74d6aac5051336987a869d2f539e6caec487
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-29 17:47:31 +0300

    component Cell

---
+++ tic-tac-toe-react/src/App.js
@@ -0,0 +1,23 @@
+import Cell from './components/Cell.js';
+import { useState } from 'react';
+
+const App = () => {
+    const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+    const [go, setGo] = useState('circle'); // the first person to go will be the one who uses circle
+    const [winningMessage, setWinningMessage] = useState(null);
+
+    const message = 'It is ' + go + "'s turn.";
+
+    return (
+        <div className="app">
+            <div className="gameboard">
+                {cells.map((cell, index) => (
+                    <Cell key={index} id={index} cell={cell} />
+                ))}
+            </div>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default App;
